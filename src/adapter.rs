b57c1ed072332bf835fd57381eda7620042a9e55
tracktwo <jcemmett1@gmail.com>
2023-02-05 11:44:41 -0500

    Cleanup response handling

--- src/adapter.rs
+++ src/adapter.rs
@@ -1,49 +1,89 @@
-
-use dap::{prelude::*, requests::{InitializeArguments, SetBreakpointsArguments, AttachRequestArguments}, types::Capabilities, responses::ErrorMessage};
+use dap::{
+    prelude::*,
+    requests::{InitializeArguments, SetBreakpointsArguments},
+    responses::ErrorMessage,
+    types::Capabilities,
+};
 use thiserror::Error;
 
 pub struct UnrealscriptAdapter;
 
 #[derive(Error, Debug)]
 pub enum UnrealscriptAdapterError {
-    #[error("Unhandled command")]
-    UnhandledCommandError,
+    #[error("Unhandled command: {0}")]
+    UnhandledCommandError(String),
+}
+
+impl UnrealscriptAdapterError {
+    fn id(&self) -> i64 {
+        match self {
+            UnrealscriptAdapterError::UnhandledCommandError(_) => 1,
+        }
+    }
+
+    pub fn to_error_message(&self) -> ErrorMessage {
+        ErrorMessage {
+            id: self.id(),
+            format: self.to_string(),
+            show_user: true,
+        }
+    }
 }
 
 impl Adapter for UnrealscriptAdapter {
     type Error = UnrealscriptAdapterError;
-    fn accept(&mut self, request: Request, ctx: &mut dyn Context) -> Result<Response,Self::Error> {
+    fn accept(&mut self, request: Request, ctx: &mut dyn Context) -> Result<Response, Self::Error> {
         log::info!("Got request {request:#?}");
-        match &request.command {
-            Command::Initialize(args) => Ok(initialize(args, &request, ctx)),
-            Command::SetBreakpoints(args) => Ok(set_breakpoints(args, &request, ctx)),
-            Command::Attach(args) => Ok(attach(args, &request, ctx)),
-            _ => Ok(Response::make_error(ctx.next_seq(), &request, ErrorMessage::new(1, "Unsupported Command", true)))
+        let response = match &request.command {
+            Command::Initialize(args) => self.initialize(args),
+            Command::SetBreakpoints(args) => self.set_breakpoints(args),
+            Command::Attach(_) => {
+                return Ok(
+                    Response::make_ack(ctx.next_seq(), &request).expect("attach can be acked")
+                )
+            }
+            _ => Err(UnrealscriptAdapterError::UnhandledCommandError(
+                request.command.name().to_string(),
+            )),
+        };
+
+        match response {
+            Ok(body) => Ok(Response::make_success(ctx.next_seq(), &request, body)),
+            Err(e) => Ok(Response::make_error(
+                ctx.next_seq(),
+                &request,
+                e.to_error_message(),
+            )),
         }
     }
 }
 
-fn initialize(_args: &InitializeArguments, request: &Request, ctx: &mut dyn Context) -> Response {
-    Response::make_success(ctx.next_seq(), request, ResponseBody::Initialize(Some(Capabilities {
-        supports_configuration_done_request: Some(true),
-        supports_delayed_stack_trace_loading: Some(true),
-        supports_value_formatting_options: Some(true),
-        ..Default::default()
-    })))
-}
-
-fn set_breakpoints(args: &SetBreakpointsArguments, request: &Request, ctx: &mut dyn Context) -> Response {
-    log::info!("Set breakpoints request");
-    if let Some(breakpoints) = &args.breakpoints {
-        for bp in breakpoints {
-            // Ask the debugger to set each breapoint in turn, and wait for the corresponding
-            // response message to be delivered.
-        }
+impl UnrealscriptAdapter {
+    fn initialize(
+        &self,
+        _args: &InitializeArguments,
+    ) -> Result<ResponseBody, UnrealscriptAdapterError> {
+        Ok(ResponseBody::Initialize(Some(Capabilities {
+            supports_configuration_done_request: Some(true),
+            supports_delayed_stack_trace_loading: Some(true),
+            supports_value_formatting_options: Some(true),
+            ..Default::default()
+        })))
     }
-    Response::make_error(ctx.next_seq(), request, ErrorMessage::new(2, "Not implemented", true) )
-}
 
-fn attach(_args: &AttachRequestArguments, request: &Request, ctx: &mut dyn Context) -> Response {
-    log::info!("Attach request");
-    Response::make_ack(ctx.next_seq(), &request).expect("Attach can be acked")
+    fn set_breakpoints(
+        &self,
+        args: &SetBreakpointsArguments,
+    ) -> Result<ResponseBody, UnrealscriptAdapterError> {
+        log::info!("Set breakpoints request");
+        if let Some(breakpoints) = &args.breakpoints {
+            for bp in breakpoints {
+                // Ask the debugger to set each breapoint in turn, and wait for the corresponding
+                // response message to be delivered.
+            }
+        }
+        Err(UnrealscriptAdapterError::UnhandledCommandError(
+            "setBreakpoints".to_string(),
+        ))
+    }
 }
