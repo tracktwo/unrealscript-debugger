89e4e84ab35e42d7007830c8ff61d09da0c4c3a4
tracktwo <jcemmett1@gmail.com>
2023-02-05 12:41:26 -0500

    Move the server into a separate thread

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io::BufReader;
 
 use adapter::UnrealscriptAdapter;
 use dap::prelude::*;
-use flexi_logger::{Logger, FileSpec};
+use flexi_logger::{FileSpec, Logger};
 
 pub mod adapter;
 pub mod client;
@@ -10,15 +10,31 @@ pub mod client;
 use client::UnrealscriptClient;
 
 fn main() {
+    let _logger = Logger::try_with_env_or_str("trace")
+        .unwrap()
+        .log_to_file(FileSpec::default().directory(
+            "C:\\users\\jonat\\projects\\debugger\\unrealscript-debugger-interface\\logs",
+        ))
+        .start()
+        .unwrap();
+
     let adapter = UnrealscriptAdapter {};
-    let client = UnrealscriptClient::new(std::io::stdout()); 
+    let client = UnrealscriptClient::new(std::io::stdout());
     let mut server = Server::new(adapter, client);
-    let _logger = Logger::try_with_env_or_str("trace").unwrap()
-        .log_to_file(FileSpec::default().directory("C:\\users\\jonat\\projects\\debugger\\unrealscript-debugger-interface\\logs"))
-        .start().unwrap();
+
+    let _event_sender = server.clone_sender();
     log::info!("Ready to start!");
-    let mut reader = BufReader::new(std::io::stdin());
-    match server.run(&mut reader) {
+
+    // Spawn a new thread for the server to process messages in. This will loop
+    // until the debugger quits.
+    let server_thread = std::thread::spawn(move || {
+        let mut reader = BufReader::new(std::io::stdin());
+        server.run(&mut reader)
+    });
+
+    // Wait for the server to finish processing. We may not necessarily get back here at
+    // all: the client may kill the adapter process if it hits certain errors.
+    match server_thread.join().unwrap() {
         Ok(()) => std::process::exit(0),
         Err(err) => {
             log::error!("Debugger failed with error {err}");
