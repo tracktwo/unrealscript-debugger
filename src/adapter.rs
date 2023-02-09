7b785a9039c12d1d030ace53278c66615b4d6b57
tracktwo <jcemmett1@gmail.com>
2023-02-08 20:14:02 -0500

    Implement the client<->adapter breakpoint handling

--- src/adapter.rs
+++ src/adapter.rs
@@ -7,18 +7,26 @@ use dap::{
     prelude::*,
     requests::{InitializeArguments, SetBreakpointsArguments},
     responses::ErrorMessage,
-    types::Capabilities,
+    types::{Capabilities, Source},
 };
 use thiserror::Error;
 
+use crate::ipc::{Breakpoint, UnrealChannel};
+
 pub struct UnrealscriptAdapter {
     class_map: BTreeMap<String, ClassInfo>,
+    channel: UnrealChannel,
+    // If true (the default and Unreal's native mode) the client expects lines to start at 1.
+    // Otherwise they start at 0.
+    one_based_lines: bool,
 }
 
 impl UnrealscriptAdapter {
     pub fn new() -> UnrealscriptAdapter {
         UnrealscriptAdapter {
             class_map: BTreeMap::new(),
+            channel: UnrealChannel::new(),
+            one_based_lines: true,
         }
     }
 }
@@ -58,6 +66,10 @@ impl Adapter for UnrealscriptAdapter {
         let response = match &request.command {
             Command::Initialize(args) => self.initialize(args),
             Command::SetBreakpoints(args) => self.set_breakpoints(args),
+            Command::ConfigurationDone => {
+                return Ok(Response::make_ack(ctx.next_seq(), &request)
+                    .expect("ConfigurationDone can be acked"))
+            }
             Command::Attach(_) => {
                 return Ok(
                     Response::make_ack(ctx.next_seq(), &request).expect("attach can be acked")
@@ -80,10 +92,17 @@ impl Adapter for UnrealscriptAdapter {
 }
 
 impl UnrealscriptAdapter {
+    /// Handle an initialize request
     fn initialize(
-        &self,
-        _args: &InitializeArguments,
+        &mut self,
+        args: &InitializeArguments,
     ) -> Result<ResponseBody, UnrealscriptAdapterError> {
+        // If the client sends linesStartAt1: false then we need to adjust
+        // all the line numbers we receive.
+        if let Some(false) = args.lines_start_at1 {
+            self.one_based_lines = false;
+        }
+
         Ok(ResponseBody::Initialize(Some(Capabilities {
             supports_configuration_done_request: Some(true),
             supports_delayed_stack_trace_loading: Some(true),
@@ -92,6 +111,7 @@ impl UnrealscriptAdapter {
         })))
     }
 
+    /// Handle a setBreakpoints request
     fn set_breakpoints(
         &mut self,
         args: &SetBreakpointsArguments,
@@ -105,19 +125,64 @@ impl UnrealscriptAdapter {
             .path
             .as_ref()
             .expect("Clients should provide sources as paths");
-        let class_info = ClassInfo::make(path.to_string()).or(Err(Error::InvalidFilename(path.to_string())))?;
+        let class_info =
+            ClassInfo::make(path.to_string()).or(Err(Error::InvalidFilename(path.to_string())))?;
         let mut class_name = class_info.qualify();
         class_name.make_ascii_uppercase();
-        self.class_map.entry(class_name).or_insert(class_info);
+        let class_info = self
+            .class_map
+            .entry(class_name.clone())
+            .or_insert(class_info);
+        // Remove all the existing breakpoints from this class.
+        for bp in class_info.breakpoints.iter() {
+            let removed = self
+                .channel
+                .remove_breakpoint(Breakpoint::new(&class_name, *bp));
+            assert!(removed.line == *bp);
+        }
 
+        class_info.breakpoints.clear();
+
+        let mut dap_breakpoints: Vec<dap::types::Breakpoint> = Vec::new();
+
+        // Now add the new ones (if any)
         if let Some(breakpoints) = &args.breakpoints {
-            for _bp in breakpoints {
-                // Ask the debugger to set each breapoint in turn, and wait for the corresponding
-                // response message to be delivered.
+            for bp in breakpoints {
+                // TODO Handle 0-or-1 line numbering.
+                // Note that Unreal only accepts 32-bit lines.
+                if let Ok(mut line) = bp.line.try_into() {
+                    // The line number received may require adjustment
+                    line += if self.one_based_lines { 0 } else { 1 };
+
+                    let new_bp = self
+                        .channel
+                        .add_breakpoint(Breakpoint::new(&class_name, line));
+
+                    // Record this breakpoint in our data structure
+                    class_info.breakpoints.push(new_bp.line);
+
+                    // Record it in the response
+                    dap_breakpoints.push(dap::types::Breakpoint {
+                        verified: true,
+                        // Line number may require adjustment before sending back out to the
+                        // client.
+                        line: Some(
+                            (new_bp.line + if self.one_based_lines { 0 } else { -1 }).into(),
+                        ),
+                        source: Some(Source {
+                            path: Some(path.to_string()),
+                            ..Default::default()
+                        }),
+                        ..Default::default()
+                    });
+                }
             }
         }
-        Err(UnrealscriptAdapterError::UnhandledCommandError(
-            "setBreakpoints".to_string(),
+
+        Ok(ResponseBody::SetBreakpoints(
+            responses::SetBreakpointsResponse {
+                breakpoints: dap_breakpoints,
+            },
         ))
     }
 }
@@ -136,10 +201,14 @@ pub struct ClassInfo {
 pub struct BadFilenameError;
 
 impl ClassInfo {
-
     pub fn make(file_name: String) -> Result<ClassInfo, BadFilenameError> {
         let (package_name, class_name) = split_source(&file_name)?;
-        Ok(ClassInfo { file_name, package_name, class_name, breakpoints: Vec::new() })
+        Ok(ClassInfo {
+            file_name,
+            package_name,
+            class_name,
+            breakpoints: Vec::new(),
+        })
     }
     /// Return a string containing a qualified classname: "package.class"
     pub fn qualify(&self) -> String {
@@ -167,12 +236,8 @@ pub fn split_source(path_str: &str) -> Result<(String, String), BadFilenameError
     // strip.
     let component = iter.next().ok_or(BadFilenameError)?;
     let class_name = match component {
-        Component::Normal(file_name) => {
-            Path::new(file_name)
-                .file_stem()
-                .ok_or(BadFilenameError)
-        }
-        _ => Err(BadFilenameError)
+        Component::Normal(file_name) => Path::new(file_name).file_stem().ok_or(BadFilenameError),
+        _ => Err(BadFilenameError),
     }?
     .to_str()
     .expect("Source path should be valid utf-8")
@@ -201,7 +266,8 @@ mod tests {
 
     #[test]
     fn can_split_source() {
-        let (package,class) = split_source("C:\\foo\\src\\MyPackage\\classes\\SomeClass.uc").unwrap();
+        let (package, class) =
+            split_source("C:\\foo\\src\\MyPackage\\classes\\SomeClass.uc").unwrap();
         assert_eq!(package, "MyPackage");
         assert_eq!(class, "SomeClass");
     }
@@ -209,10 +275,7 @@ mod tests {
     #[test]
     fn split_source_bad_classname() {
         let info = split_source("C:\\MyMod\\BadClass.uc");
-        assert!(matches!(
-            info,
-            Err(BadFilenameError)
-        ));
+        assert!(matches!(info, Err(BadFilenameError)));
     }
 
     #[test]
