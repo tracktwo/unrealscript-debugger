497da256eb86590a9b0f2bda0397e6db24f06850
tracktwo <jcemmett1@gmail.com>
2023-02-27 20:54:02 -0500

    Another variables test

--- integration_tests/variables.rs
+++ integration_tests/variables.rs
@@ -4,7 +4,7 @@ use common::{FrameIndex, UnrealCommand, VariableIndex, WatchKind};
 use dap::prelude::*;
 
 #[test]
-fn frame_0_is_default() {
+fn frame_0() {
     let (mut adapter, mut client, _receiver, handle) = fixture::setup(|dbg, deserializer| {
         dbg.add_frame("MyPackage.MyClass\0".as_ptr() as *const i8);
         dbg.add_watch(
@@ -56,5 +56,63 @@ fn frame_0_is_default() {
     fixture::wait_for_thread(handle);
 }
 
-// TODO Test switching stacks
+#[test]
+/// Test a variables request for a variable in a frame other than the top-most.
+fn frame_2() {
+    let (mut adapter, mut client, _receiver, handle) = fixture::setup(|dbg, deserializer| {
+        dbg.add_frame("Function MyClass.TopFunction\0".as_ptr() as *const i8);
+        dbg.add_frame("Function MyClass.CallingFunction\0".as_ptr() as *const i8);
+        dbg.add_frame("Function AnotherClass.AnotherCaller\0".as_ptr() as *const i8);
+        // Next command should be a variables request.
+        let command = deserializer.next().unwrap().unwrap();
+
+        assert!(matches!(command, UnrealCommand::Variables(_, _, _, _, _)));
+        dbg.handle_command(command).unwrap();
+
+        // This should have resulted in a frame switch request. Add the watch
+        // and then send an unlock to unblock the caller.
+        dbg.add_watch(
+            WatchKind::Local,
+            -1,
+            "SomeVar ( Int,02392392,032932 )\0".as_ptr() as *const i8,
+            "33\0".as_ptr() as *const i8,
+        );
+        dbg.unlock_watchlist(WatchKind::User);
+    });
+
+    // Ask for variable 0 in frame 2.
+    let response = adapter
+        .accept(
+            Request {
+                seq: 3,
+                command: Command::Variables(requests::VariablesArguments {
+                    variables_reference: VariableReference::new(
+                        WatchKind::Local,
+                        FrameIndex::create(2).unwrap(),
+                        VariableIndex::SCOPE,
+                    )
+                    .to_int(),
+                    filter: None,
+                    start: Some(0),
+                    count: Some(0),
+                    format: None,
+                }),
+            },
+            &mut client,
+        )
+        .unwrap();
+
+    match response.body.unwrap() {
+        ResponseBody::Variables(resp) => {
+            assert_eq!(resp.variables.len(), 1);
+            let v = &resp.variables[0];
+            assert_eq!(v.name, "SomeVar");
+            assert_eq!(v.type_field.as_ref().unwrap(), "Int");
+            assert_eq!(v.value, "33");
+        }
+        o => assert!(false, "Expected a variables response: {o:#?}"),
+    }
+
+    fixture::wait_for_thread(handle);
+}
 // TODO Test source roots.
