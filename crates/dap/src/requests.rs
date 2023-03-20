0f4734f9c16cb5afc099fa91e8e84b8a1ef3b93c
tracktwo <tracktwo@users.noreply.github.com>
2023-03-19 21:48:01 -0400

    add ci (#5)

--- crates/dap/src/requests.rs
+++ crates/dap/src/requests.rs
@@ -1,35 +1,65 @@
+//! DAP requests
+//!
+//! Requests are sent from the client (editor) to the adapter and expect a
+//! response.
+
 use serde::Deserialize;
 use strum::Display;
 
 use crate::types::{Source, SourceBreakpoint};
 
+/// A request from the DAP client.
 #[derive(Deserialize, Debug)]
 #[serde(tag = "type", rename = "request")]
 pub struct Request {
+    /// The sequence number for the request.
     pub seq: i64,
+    /// The request command.
     #[serde(flatten)]
     pub command: Command,
 }
 
+/// The DAP request 'command' or type.
 #[derive(Deserialize, Debug, Display)]
 #[serde(tag = "command", content = "arguments", rename_all = "camelCase")]
 #[strum(serialize_all = "camelCase")]
 pub enum Command {
+    /// Attach to a running process.
     Attach(AttachArguments),
+    /// The client has finished the configuration stage.
     ConfigurationDone,
+    /// Continue execution.
     Continue(IgnoredArguments),
+    /// Disconnect from the debuggee. We treat this as shutting down the
+    /// debugging session. If we launched the debuggee it will close the process
+    /// too.
     Disconnect(IgnoredArguments),
+    /// Evaluate a given watch expression.
     Evaluate(EvaluateArguments),
+    /// Initialize the connection with the client. Contains configuration details
+    /// about the client.
     Initialize(InitializeArguments),
+    /// Launch an application and optionally debug it.
     Launch(LaunchArguments),
+    /// Step over the next statement.
     Next(IgnoredArguments),
+    /// Tell the debuggee to break.
     Pause(IgnoredArguments),
+    /// Request for scope information. Unrealscript has only two real scopes: local scope
+    /// and global (class) scope.
     Scopes(ScopesArguments),
+    /// Set breakpoints for a given file. This completely replaces all previous breakpoints
+    /// in the file.
     SetBreakpoints(SetBreakpointsArguments),
+    /// Request stack trace information.
     StackTrace(StackTraceArguments),
+    /// Step into the next statement.
     StepIn(IgnoredArguments),
+    /// Step out of the current function.
     StepOut(IgnoredArguments),
+    /// Request information about the currently running threads. Unreal has only a single thread.
     Threads,
+    /// Request information about variables.
     Variables(VariablesArguments),
 }
 
@@ -41,68 +71,147 @@ pub enum Command {
 #[derive(Deserialize, Debug)]
 pub struct IgnoredArguments {}
 
+/// Arguments for an [`Command::Attach`] command.
+///
+/// These are almost entirely implementation-defined, and are usually populated
+/// from the launch configuration of the editor.
 #[derive(Deserialize, Debug)]
 pub struct AttachArguments {
+    /// The port number to use for communication with the interface. If not specified
+    /// a default port number will be used.
     pub port: Option<u16>,
+    /// An ordered list of directories in which to search for source files. This is required
+    /// so that we can tell the editor what file to open when the debugger breaks in some
+    /// Unreal class. It relies on the naming and directory layout convention of Unreal so
+    /// we can map a package and class name to a source file.
     pub source_roots: Option<Vec<String>>,
+    /// If true enable the 'stack hack', an experimental feature to provide full line information
+    /// for all frames in a stack trace. By default Unreal only provides line information for
+    /// the top-most entry of the stack, but DAP and most editors want to know the line number for
+    /// each element in the trace.
     pub enable_stack_hack: Option<bool>,
 }
 
+/// Arguments for a [`Command::Evaluate`] command.
+///
+/// This is used to add watch expressions.
 #[derive(Deserialize, Debug)]
 pub struct EvaluateArguments {
+    /// The expression to evaluate.
     pub expression: String,
     #[serde(rename = "frameId")]
+    /// The id of the frame in which this expression should be evaluated.
     pub frame_id: Option<i64>,
 }
 
+/// Arguments for a [`Command::Initialize`] command.
+///
+/// This command has configuration details for the client.
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct InitializeArguments {
+    /// Lines start at 1 (true or unset) or 0 (false).
     pub lines_start_at1: Option<bool>,
+
+    /// If true the client supports 'type' fields in variables. If this is not
+    /// set to true we will not send type info as part of variables respoonses.
     pub supports_variable_type: Option<bool>,
+
+    /// If true the client supports 'invalidated' events. If not set to true
+    /// we will not send [`crate::events::EventBody::Invalidated`] events when switching
+    /// stack frames. In such an editor we will not have line information for any
+    /// stack frame other than the top-most unless the stack hack is enabled.
     pub supports_invalidated_event: Option<bool>,
 }
 
+/// Arguments for a [`Command::Launch`] request.
+///
+/// This is sent as part of initialization when the client wants us to launch
+/// a process to debug.
+///
+/// All arguments are implementation-defined.
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct LaunchArguments {
+    /// The port to use to communicate with the interface.
     pub port: Option<u16>,
+    /// If `true` we will launch but not debug the program.
     pub no_debug: Option<bool>,
+    /// The list of source roots. See [`AttachArguments::source_roots`].
     pub source_roots: Option<Vec<String>>,
+    /// Enable the stack hack. See [`AttachArguments::enable_stack_hack`].
     pub enable_stack_hack: Option<bool>,
+    /// Full path to the program to launch.
     pub program: Option<String>,
+    /// An array of arguments to pass to the program.
     pub args: Option<Vec<String>>,
 }
 
+/// Arguments for a [`Command::Scopes`] request.
+///
+/// The client requests this when it wants to display variable information
+/// for a particular stack frame. This occurs when we first break for the
+/// topmost frame and then whenever we switch frames in the editor.
+///
+/// Scopes in Unrealscript are limited and are always just two: the locals
+/// scope and the globals scope (class scope).
 #[derive(Deserialize, Debug)]
 pub struct ScopesArguments {
+    /// The frame id to request scope info for.
     #[serde(rename = "frameId")]
     pub frame_id: i64,
 }
 
+/// Arguments for a [`Command::SetBreakpoints`] request.
+///
+/// This is used to set breakpoints in the given file. Each time this
+/// request is processed the list of breakpoints sent completely replaces
+/// any previous breakpoints, e.g. removing the last breakpoint in the file
+/// will send a set breakpoints request with an empty breakpoint list.
 #[derive(Deserialize, Debug)]
 pub struct SetBreakpointsArguments {
+    /// The source file for which to add breakpoints.
     pub source: Source,
+    /// The complete list of breakpoints for this file.
     pub breakpoints: Option<Vec<SourceBreakpoint>>,
 }
 
+/// Arguments for a [`Command::StackTrace`] request.
+///
+/// This requests stack information and is usually requested each time the
+/// debugger breaks.
+///
+/// DAP numbers stack frames starting from 0 as the top-most frame and works
+/// down the stack with increasing numbers.
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct StackTraceArguments {
+    /// The thread ID we are requesting info for.
     pub thread_id: i64,
+    /// If set, the first frame we want info for. If not set or set to 0 we
+    /// request info starting at the top-most frame. Used with [`Self::levels`] to
+    /// implement paginated processing of frames.
     pub start_frame: Option<i64>,
+    /// If set, the number of frames to send in this response. Used with
+    /// [`Self::start_frame`] to implement paginated processing of frames.
     pub levels: Option<i64>,
 }
 
+/// Arguments for a [`Command::Variables`] request.
+///
+/// This requests information about the variables in a given scope, or the
+/// children of a variable for structured variables.
 #[derive(Deserialize, Debug)]
 pub struct VariablesArguments {
+    /// The variable reference we are requesting info for. This will represent
+    /// either a scope that we returned from a [`crate::requests::Command::Scopes`] request or
+    /// a variable we have returned from a previous `Variables` request.
     #[serde(rename = "variablesReference")]
     pub variables_reference: i64,
+    /// If set this is the index of child variables to start from. If not set
+    /// or 0 we start from the first child.
     pub start: Option<i64>,
+    /// If set this is the number of variables to request. If not set or 0
+    /// we return all children.
     pub count: Option<i64>,
 }
-
-/// Implementation-specific options for launch and attach.
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-pub struct UnrealscriptAdapterOptions {}
--- crates/dap/src/responses.rs
+++ crates/dap/src/responses.rs
@@ -1,3 +1,8 @@
+//! DAP Responses.
+//!
+//! Responses are sent for each DAP [`crate::requests::Request`] and can indicate success
+//! or some failure.
+
 use serde::Serialize;
 
 use crate::{
@@ -5,26 +10,39 @@ use crate::{
     types::{Breakpoint, Capabilities, Scope, StackFrame, Thread, Variable, VariableReferenceInfo},
 };
 
+/// The top-level protocol message for a response. This is typically used directly only by
+/// the client, the adapter builds `Response` messages.
 #[derive(Serialize)]
 #[serde(tag = "type", rename = "response")]
 pub struct ResponseMessage {
+    /// The sequence number for this response.
     pub seq: i64,
+    /// The response contents.
     #[serde(flatten)]
     pub response: Response,
 }
 
+/// A Response object. This is the top-level type directly used by the adapter to build responses
+/// to requests.
 #[derive(Serialize)]
 pub struct Response {
+    /// The sequence number of the request we are responding to.
     pub request_seq: i64,
+    /// If true the request was successfully processed.
     pub success: bool,
+    /// The request command.
     pub command: String,
+    /// If [`Self::success`] is false, an optional message to display in the client.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub message: Option<String>,
+    /// A response-specific body. Can also be error details if [`Self::success`] is false.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub body: Option<ResponseBody>,
 }
 
 impl Response {
+    /// Helper to construct a 'success' response to the given request with the
+    /// given body.
     pub fn make_success(request: &Request, body: ResponseBody) -> Self {
         Self {
             request_seq: request.seq,
@@ -35,6 +53,8 @@ impl Response {
         }
     }
 
+    /// Helper to make a simple 'ack' response. Several DAP responses have no
+    /// bodies and are simply empty ack responses.
     pub fn make_ack(request: &Request) -> Self {
         Self {
             request_seq: request.seq,
@@ -45,6 +65,8 @@ impl Response {
         }
     }
 
+    /// Helper to construct an error response to the ginen request with the given
+    /// title and body.
     pub fn make_error(request: &Request, title: String, message: MessageResponseBody) -> Self {
         Self {
             request_seq: request.seq,
@@ -56,73 +78,134 @@ impl Response {
     }
 }
 
+/// A response body. This is a request-specific variant.
 #[derive(Serialize, Debug)]
 #[serde(untagged)]
 pub enum ResponseBody {
+    /// The response to an [`crate::requests::Command::Initialize`] request.
     Initialize(Option<Capabilities>),
+    /// The response to an [`crate::requests::Command::SetBreakpoints`] request.
     SetBreakpoints(SetBreakpointsResponseBody),
+    /// The response to an [`crate::requests::Command::Continue`] request.
     Continue(ContinueResponseBody),
+    /// The response to an [`crate::requests::Command::StackTrace`] request.
     StackTrace(StackTraceResponseBody),
+    /// The response to an [`crate::requests::Command::Scopes`] request.
     Scopes(ScopesResponseBody),
+    /// The response to an [`crate::requests::Command::Variables`] request.
     Variables(VariablesResponseBody),
+    /// The response to an [`crate::requests::Command::Threads`] request.
     Threads(ThreadsResponseBody),
+    /// The response to an [`crate::requests::Command::Evaluate`] request.
     Evaluate(EvaluateResponseBody),
+    /// The response body for an error response.
     Message(MessageResponseBody),
 }
 
+/// A [`ResponseBody::SetBreakpoints`] response. Contains the list of breakpoints
+/// that was set.
+///
+/// Typically this is used to indicate the actual line a breakpoint was set on
+/// if it was automatically adjusted (e.g. for a multi-line statement) or if the
+/// breakpoint was successfully validated by the debugger. Unreal doesn't tell us
+/// any information and will happily report a breakpoint set in a completely invalid
+/// spot such as a comment. These breakpoints will simply never be hit.
 #[derive(Serialize, Debug)]
 #[serde(rename = "setBreakpoints")]
 pub struct SetBreakpointsResponseBody {
+    /// The list of breakpoints for the file in the request.
     pub breakpoints: Vec<Breakpoint>,
 }
 
+/// A [`ResponseBody::Continue`] response. Indicates whether all threads were
+/// continued or not. Since Unrealscript only has one thread this is always true.
 #[derive(Serialize, Debug)]
 #[serde(rename = "continue")]
 pub struct ContinueResponseBody {
+    /// If true all threads were continued. In this implementation this is always
+    /// true because there is only one thread.
     #[serde(rename = "allThreadsContinued")]
     pub all_threads_continued: bool,
 }
 
+/// A [`ResponseBody::StackTrace`] response. Contains information about the current
+/// call stack.
+///
+/// DAP expects the stack frame entries to have line numbers, but we only have
+/// full line info if the stack hack is enabled. If not we only have a line number
+/// for the top-most frame and all other frames have the line set to 0.
 #[derive(Serialize, Debug)]
 #[serde(rename = "stackTrace")]
 pub struct StackTraceResponseBody {
+    /// The list of stack frames.
     #[serde(rename = "stackFrames")]
     pub stack_frames: Vec<StackFrame>,
 }
 
+/// A [`ResponseBody::Scopes`] response.
+/// Contains information about the global and local scopes including an identitier to use when
+/// requesting variables in these scopes and counts of the number of variables.
 #[derive(Serialize, Debug)]
 #[serde(rename = "scopes")]
 pub struct ScopesResponseBody {
+    /// The list of scopes. In Unrealscript this will always be a 2-element
+    /// array with locals and globals.
     pub scopes: Vec<Scope>,
 }
 
+/// A [`ResponseBody::Variables`] response.
+///
+/// Contains information about the child variables for the variable reference
+/// in the request. This reference could be a scope or a variable itself.
 #[derive(Serialize, Debug)]
 #[serde(rename = "variables")]
 pub struct VariablesResponseBody {
+    /// A list of variables that are considered children of whatever variable reference
+    /// was in the request, which could be a scope or a variable itself.
     pub variables: Vec<Variable>,
 }
 
+/// A [`ResponseBody::Threads`] response.
+///
+/// Unrealscript has only a single thread, so this response always has the
+/// same content.
 #[derive(Serialize, Debug)]
 #[serde(rename = "threads")]
 pub struct ThreadsResponseBody {
+    /// The list of threads. Always a single element.
     pub threads: Vec<Thread>,
 }
 
+/// A [`ResponseBody::Evaluate`] response.
+///
+/// Evaluate requests are mapped to user watches in Unrealscript. The response
+/// has the value of the given expression as a variable.
 #[derive(Serialize, Debug)]
 #[serde(rename = "evaluate")]
 pub struct EvaluateResponseBody {
+    /// The result of the expression.
     pub result: String,
+    /// The type of the expression. Only sent if
+    /// [`crate::requests::InitializeArguments::supports_variable_type`] was sent by the client in the
+    /// initialize request.
     #[serde(rename = "type")]
     pub ty: Option<String>,
+    /// Variable reference info. This is not part of DAP but is a flattened struct that holds
+    /// pieces common to several responses.
     #[serde(flatten)]
     pub variable_info: VariableReferenceInfo,
 }
 
+/// A response body for an error response.
 #[derive(Serialize, Debug)]
 #[serde(rename = "message")]
 pub struct MessageResponseBody {
+    /// An implementation-specific ID. This number should be unique per error message
+    /// kind. Can be used to help the user look up info on the error.
     pub id: i64,
+    /// The error contents.
     pub format: String,
+    /// If true this error should be displayed to the user.
     #[serde(rename = "showUser")]
     pub show_user: bool,
 }
